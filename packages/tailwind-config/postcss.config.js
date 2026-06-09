bb478a92f80a9e0ff848e518e011c8fdfd3f5d19
zhangpeng <zhangpengtongxue@gmail.com>
2026-06-09 17:57:10 +0800

    refactor(tailwind-config): simplify PostCSS configuration by using array syntax for plugins

--- packages/tailwind-config/postcss.config.js
+++ packages/tailwind-config/postcss.config.js
@@ -1,6 +1,5 @@
+import tailwindcss from "@tailwindcss/postcss";
+
 export default {
-  plugins: {
-    // "tailwindcss/nesting": {},
-    "@tailwindcss/postcss": {},
-  },
+  plugins: [tailwindcss()],
 };
